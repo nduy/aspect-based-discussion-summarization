Fix normalization error

AGmodel.py
@@ -11,6 +11,7 @@ from nltk.tokenize import word_tokenize
 import warnings
 from itertools import permutations
 from sklearn import metrics
+from config import model_build_options as options
 
 
 class AGmodel:
@@ -99,7 +100,8 @@ class AGmodel:
             maybe_print(u' -> Topic: {0}, Nodes: {1}'.format(key,list(self.topic_graphs[key].nodes())), 3, 'i')
 
         # Compute matrix
-        self.matrix = self.compute_probability_matrix(method='eigenvector_centrality', enable_softmax=True)
+        self.matrix = self.compute_probability_matrix(method=options.get('centrality_method'),
+                                                      normalize=options.get('normalization_method'))
         # print(self.matrix)
 
     @classmethod
@@ -134,7 +136,7 @@ class AGmodel:
         """
         return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-    def compute_probability_matrix(self, method='eigenvector_centrality', enable_softmax=False):
+    def compute_probability_matrix(self, method='eigenvector_centrality', normalize='sum'):
         """
         Compute the probability matrix
         :param method: threee methods:
@@ -143,38 +145,52 @@ class AGmodel:
             - 'pagerank': bases on result of page rank algorithm
             - 'eigenvector_centrality': Eigenvector centrality computes the centrality for a node based on
             the centrality of its neighbors.
-        :param enable_softmax: apply softmax function on computing probability from node characterize statistic
-            - True: apply softmax
-            - False: apply simple scale by sum
+        :param normalize: apply normaliztion on centrality score, so that they sum to 1
+            - 'softmax': apply softmax function on computing probability from node characterize statistic
+            - 'sum': apply simple scale by sum of all centrality scores
         :return:
         """
         result_matrix = None
-        if method == 'eigenvector_centrality':
-            # for each topic, we compute a vector size len(dictionary)
-            # print '----->',self.topic_ids
-            for key in self.topic_ids:
-                # print len(self.topic_graphs[key].edges())
+
+        # for each topic, we compute a vector size len(dictionary)
+        # print '----->',self.topic_ids
+        for key in self.topic_ids:
+            # print len(self.topic_graphs[key].edges())
+            if method == 'eigenvector':
                 centrality = nx.eigenvector_centrality(G=self.topic_graphs[key], max_iter=10000)
-                # print centrality
-                row = np.zeros(self.dict_size)
-                for node_id in centrality:
-                    # row[self.inverted_node_id_dictionary[node_id]] = centrality[node_id]
-                    for i in self.inverted_node_id_dictionary[node_id]:
-                        row[i] = centrality[node_id]
-                if result_matrix is None:
-                    result_matrix = row
-                else:
-                    result_matrix = np.vstack((result_matrix, row))
+            elif method == 'degree':
+                centrality = nx.degree_centrality(G=self.topic_graphs[key])
+            elif method == 'closeness':
+                centrality = nx.closeness_centrality(G=self.topic_graphs[key])
+            elif method == 'betweenness':
+                centrality = nx.betweenness_centrality(G=self.topic_graphs[key])
+            elif method == 'pagerank':
+                centrality = nx.pagerank(G=self.topic_graphs[key])
+            else:
+                raise ValueError('Invalid centrality method: {0}'.format(method))
+            # print centrality
+            row = np.zeros(self.dict_size)
+            for node_id in centrality:
+                # row[self.inverted_node_id_dictionary[node_id]] = centrality[node_id]
+                for i in self.inverted_node_id_dictionary[node_id]:
+                    row[i] = centrality[node_id]
+            if result_matrix is None:
+                result_matrix = row
+            else:
+                result_matrix = np.vstack((result_matrix, row))
+
         # Convert characterize statistic to probability
         # print result_matrix
-        if not enable_softmax: # divide each element in a row a sum of that row
+        if normalize == "softmax": # divide each element in a row a sum of that row
             # now result_matrix is just a sum matrix. We need to turn it to a normaized matrix
             row_sum = np.sum(result_matrix,axis=1)[np.newaxis] # Sum by row. Result an array size is number of topics
             row_sum = row_sum.T     # convert to matrix for division
             result_matrix = result_matrix/np.repeat(row_sum, self.dict_size,axis=1)
-        else:  # apply softmax to each row of the matrix
+        elif normalize == "sum":  # apply softmax to each row of the matrix
             for i in xrange(0,self.n_topics):
                 result_matrix[i] = self.softmax(result_matrix[i])
+        else:
+            raise ValueError('Invalid normalization method: {0}'.format(normalize))
         return result_matrix
 
     def predict_doc(self, doc_id=None, text='', vectorize_method='bow', top=3):

utils.py
@@ -19,7 +19,14 @@ import jsonrpc
 import json
 from collections import Counter
 from scipy.spatial.distance import cosine
+import networkx as nx
 import en
+import sys
+from operator import itemgetter
+import csv
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Normalization and cleaning engine
 cucco = Cucco()
@@ -35,6 +42,29 @@ normalizations = [
     'remove_extra_whitespaces',
 ]
 
+rep = {u'\n':'' ,
+       '            ': '',
+       u"\n \n": "",
+       u" \t  \t ": "",
+       u"\u201c": "\"",
+       u"\u201d": "\"",
+       u"\u2019": "\'",
+       u"\u2018": "\'",
+       u"\u2013": "-",
+       u"\u2014": "-",
+       u"\u2011": "-",
+#       u"\u0027": "",
+#       u"\u0022": "",
+       u"\u201e": "",
+       u"\u201d": "",
+#       u"\u002c": "",
+       u"\u002b": "",
+       u"\u07f5": "",
+       u"\u07f4": "",
+       u"\u0092": ""}
+
+rep = dict((re.escape(k), v) for k, v in rep.iteritems())
+pattern = re.compile("|".join(rep.keys()))
 
 # Decide to print the print text or not according to the verbality
 # command_verbality:
@@ -44,7 +74,7 @@ normalizations = [
 def maybe_print(text, command_verbality=1, alias=" "):
     if script_verbality > 0:
         if script_verbality >= command_verbality:
-            print '[{0}] {1}'.format(alias,text)
+            print u'[{0}] {1}'.format(alias,text.encode('utf-8'))
 
 
 # flattening list of sublist into a single list
@@ -123,17 +153,7 @@ def gen_mcs_only():
 # @param: a string, should be in unicode
 # @return: the normalized string
 def simple_normalize(cnt):
-    reps = ('\n', ''), \
-           ('            ', ''), \
-           ("\n \n", ""), \
-           (u"\u201c", "\""), \
-           (u"\u201d", "\""), \
-           (u"\u2019", "\'"), \
-           (u"\u2018", "\'"), \
-           (u"\u2013", "-"), \
-           (u"\u2014", "-"), \
-           (u"\u2011", "-")
-    return reduce(lambda a, kv: a.replace(*kv), reps, cnt)
+    return pattern.sub(lambda m: rep[re.escape(m.group(0))], cnt)
 
 
 # Segmentize the sentences
@@ -152,6 +172,9 @@ def texttiling_tokenize(sentence_list):
     # print doc
     tt = texttiling.TextTilingTokenizer()
     segmented_text = tt.tokenize(doc)
+    #print segmented_text
+    # print [simple_normalize(para.strip()) for para in segmented_text if para.strip()]
+    # exit(0)
     return [simple_normalize(para.strip()) for para in segmented_text if para.strip()]
 
 
@@ -171,12 +194,14 @@ def replace_all(repls, str):
 #text =  "i like apples, but pears scare me"
 #print replace_all({"apple": "pear", "pear": "apple"}, text)
 
+
 # Clean up the text, and get rig of unnecessary characters
 def text_preprocessing(rawText):
     # print  replace_pattern
-    txt = cucco.normalize(rawText, normalizations)
+    txt = cucco.normalize(rawText.decode('utf8', 'ignore'), normalizations)
     txt = re.sub('<\D*>.*?<\D*>', '', txt)
-    m = re.search(r"[a-z]([.,;:<>()+])[A-Z]", txt) # Fix space missing typo
+    # m = re.search(r"[a-z]([.,;:<>()+])[A-Z]", txt) # Fix space missing typo
+    m = re.search(r"[a-z]([<>()+])[A-Z]", txt) # Fix space missing typo
     txt = txt[:m.start()+2] + u" " + txt[m.end()-1:] if m else txt
     txt = replace_all(replace_pattern,txt)
     return txt
@@ -306,4 +331,41 @@ def repetition_summary(inp_string):
             head_str = re.sub('(<br> - ' + key + '\^[\d]+.+)<br>',u'',head_str)
             rs = rs + u"<br> » " + key + u"^" + str(count_keys[key])
     return head_str + rs
+
+
+def print_top_keyphrases(g=None, ntop=20, out_path='./top20.csv'):
+    """
+    :param g: the aspect graph to compute
+    :param ntop: number of topwords to extract
+    :param out_path: path of output csv file
+    :return: None
+    """
+    assert g is not None, "Can't get keyphrase from a NoneType graph."
+    eigenvector_cen = nx.eigenvector_centrality(G=g, max_iter=10000)
+    eigenvector_top = sorted(eigenvector_cen.iteritems(), key=itemgetter(1), reverse=True)[:min([ntop,
+                                                                                                len(eigenvector_cen)])]
+
+    degree_cen = nx.degree_centrality(G=g)
+    degree_top = sorted(degree_cen.iteritems(), key=itemgetter(1), reverse=True)[:min(ntop, len(degree_cen))]
+
+    closeness_cen = nx.closeness_centrality(G=g)
+    closeness_top = sorted(closeness_cen.iteritems(), key=itemgetter(1), reverse=True)[:min([ntop, len(closeness_cen)])]
+
+    betweenness_cen = nx.betweenness_centrality(G=g)
+    betweenness_top = sorted(betweenness_cen.iteritems(), key=itemgetter(1), reverse=True)[:min([ntop,
+                                                                                                len(betweenness_cen)])]
+
+    pagerank_cen = nx.pagerank(G=g)
+    pagerank_top = sorted(pagerank_cen.iteritems(), key=itemgetter(1), reverse=True)[:min([ntop, len(pagerank_cen)])]
+
+    # Now write to filec
+    with open(out_path, 'wb+') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Eigenvector_cen', 'Degree', 'Closeness', 'Betweenness', 'Pagerank'])
+        for i in xrange(0,ntop):
+            writer.writerow([g.node[eigenvector_top[i][0]]['label'] if i < len(eigenvector_top) else "",
+                             g.node[degree_top[i][0]]['label'] if i < len(degree_top) else "",
+                             g.node[closeness_top[i][0]]['label'] if i < len(closeness_top) else "",
+                             g.node[betweenness_top[i][0]]['label'] if i < len(betweenness_top) else "",
+                             g.node[pagerank_top[i][0]]['label']]) if i < len(pagerank_top) else ""
 